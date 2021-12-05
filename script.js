e41857c2005bb5824d6f06e30ad4793a7a0d9549
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2021-12-05 13:56:45 -0800

    poision dart frog idea

--- script.js
+++ script.js
@@ -1,55 +1,15 @@
-let snowflakes = []; // array to hold snowflake objects
+let frog;
+
+function preload() {
+  frog = loadImage('assets/frog.gif')
+};
 
 function setup() {
-  fill(240);
-  noStroke();
-}
+  createCanvas(windowWidth, windowHeight);
+  let x = auto(width);
+  let y = auto(length);
+};
 
-function draw() {
-  background('lightblue');
-  let t = frameCount / 120; // update time
-
-  // create a random number of snowflakes each frame
-  for (let i = 0; i < random(5); i++) {
-    snowflakes.push(new snowflake()); // append snowflake object
-  }
-
-  // loop through snowflakes with a for..of loop
-  for (let flake of snowflakes) {
-    flake.update(t); // update snowflake position
-    flake.display(); // draw snowflake
-  }
-}
-
-// snowflake class
-function snowflake() {
-  // initialize coordinates
-  this.posX = 0;
-  this.posY = random(-50, 0);
-  this.initialangle = random(0, 2 * PI);
-  this.size = 0.5
-
-  // radius of snowflake spiral
-  // chosen so the snowflakes are uniformly spread out in area
-  this.radius = sqrt(random(pow(width / 2, 2)));
-
-  this.update = function(time) {
-    // x position follows a circle
-    let w = 0.1; // angular speed
-    let angle = w * time + this.initialangle;
-    this.posX = width / 2 + this.radius * sin(angle);
-
-    // different size snowflakes fall at slightly different y speeds
-    this.posY += pow(this.size, 0.5);
-
-    // delete snowflake if past end of screen
-    if (this.posY > height) {
-      let index = snowflakes.indexOf(this);
-      snowflakes.splice(index, 1);
-    }
-  };
-
-  this.display = function() {
-    ellipse(this.posX, this.posY, this.size);
-  };
-}
+Show(); {
+image(frog, this.x, this.y);
+};
